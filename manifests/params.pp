e03dc28099171c26f9f7218960808a03261c602b
efoft <pajan@yandex.ru>
2019-08-04 19:22:12 +0300

    package name differs depending on OS version

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,11 +3,21 @@ class fail2ban::params {
 
   case $::osfamily {
     'redhat' : {
-      $package_name  = 'fail2ban-server'
       $service_name  = 'fail2ban'
       $jail_d_path   = '/etc/fail2ban/jail.d'
       $filter_d_path = '/etc/fail2ban/filter.d'
       $cfgfile       = '/etc/fail2ban/jail.local'
+      case $::operatingsystemmajrelease {
+        '6' : {
+          $package_name = 'fail2ban'
+        }
+        '7' : {
+          $package_name = 'fail2ban-server'
+        }
+        default: {
+          fail('Sorry! Your OS version is not supported')
+        }
+      }
     }
     default: {
       fail('Sorry! Your OS is not supported')
